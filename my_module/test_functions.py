1d13c4a061ce3962a7c6dc4662ffa92cf7ffdb10
farrah0228 <farrah0228@gmail.com>
2018-12-11 23:35:45 -0800

    Delete test_functions.py

--- my_module/test_functions.py
+++
@@ -1,7 +0,0 @@
-from functions import *
-def test_encoded_program():
-    assert decoded_program("Êë®¢Ë¢êñòç¢ûñ÷¢çðìñûçæ¢öêëõ¢òôñéôãï£") == ("Hi, I hope you enjoyed this program!")
-
-def test_password_func():
-    assert encoded_program("password") == ("òãõõùñôæ")
-    
